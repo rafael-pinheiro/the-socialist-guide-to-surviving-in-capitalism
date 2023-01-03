c56f1e5bfb7c6bccf520afa3ca421ce57676ee05
Rafa <pinheiro.rbp@gmail.com>
2023-01-03 18:13:06 -0300

    pagination

--- src/core/Chapter.ts
+++ src/core/Chapter.ts
@@ -4,6 +4,7 @@ export type Chapter = CollectionEntry<'chapters'>;
 export type ChaptersByLanguage = Record<string, Chapter[]>;
 
 const sort = (chapters: Chapter[]) => chapters.sort((a, b) => a.data.sortOrder - b.data.sortOrder);
+export const getLanguage = (chapter: Chapter) => chapter.slug.split("/")[0];
 
 export async function getChapters(): Promise<Chapter[]> {
   return sort(await getCollection('chapters'));
@@ -12,7 +13,7 @@ export async function getChapters(): Promise<Chapter[]> {
 export async function getByLanguage(): Promise<ChaptersByLanguage> {
   const chapters = (await getCollection("chapters")).reduce(
     (result, current) => {
-      const [lang] = current.slug.split("/");
+      const lang = getLanguage(current);
 
       result[lang] = result[lang] || [];
       result[lang].push(current);
@@ -27,4 +28,20 @@ export async function getByLanguage(): Promise<ChaptersByLanguage> {
   }
 
   return chapters;
+}
+
+export async function getPrevious(chapter: Chapter): Promise<Chapter | undefined> {
+  const lang = getLanguage(chapter);
+  return (await getCollection(
+    "chapters",
+    ({ slug, data }) => data.sortOrder === chapter.data.sortOrder - 1 && slug.startsWith(lang)
+  ))?.[0];
+}
+
+export async function getNext(chapter: Chapter): Promise<Chapter | undefined> {
+  const lang = getLanguage(chapter);
+  return (await getCollection(
+    "chapters",
+    ({ slug, data }) => data.sortOrder === chapter.data.sortOrder + 1 && slug.startsWith(lang)
+  ))?.[0];
 }
