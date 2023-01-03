96edfc39e73a4489a0b92b99242df6d2e61cf67b
Rafa <pinheiro.rbp@gmail.com>
2023-01-03 20:31:29 -0300

    testing vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +15,6 @@ export default defineConfig({
   experimental: {
     contentCollections: true
   },
+  output: 'server',
   adapter: vercel()
 });
